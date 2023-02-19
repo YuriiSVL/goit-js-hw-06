1a4d4e003bafb887797fef703f3ac168b7d4b74c
Yurii Syvolapenko <svl.yurii@gmail.com>
2023-02-19 17:14:47 +0200

    do task1, task2, task3

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,18 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+
+const ingredientListEl = document.querySelector("#ingredients");
+const ingredientItemsEl = ingredients.map((name) => {
+  const liEl = document.createElement("li");
+  liEl.textContent = name;
+  liEl.classList.add("item");
+  return liEl;
+});
+
+ingredientListEl.append(...ingredientItemsEl);
