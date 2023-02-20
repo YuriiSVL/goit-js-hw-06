90e5a8ab9309a13e163445d25c86b25991cda305
Yurii Syvolapenko <svl.yurii@gmail.com>
2023-02-20 10:23:59 +0200

    rework task 9

--- js/task-09.js
+++ js/task-09.js
@@ -4,8 +4,10 @@ function getRandomHexColor() {
 
 const changeColorBtnRef = document.querySelector(".change-color");
 const bodyRef = document.querySelector("body");
+const colorRef = document.querySelector(".color");
 changeColorBtnRef.addEventListener("click", onChangColorBtnClick);
 
 function onChangColorBtnClick() {
   bodyRef.style.backgroundColor = getRandomHexColor();
+  colorRef.textContent = getRandomHexColor();
 }
