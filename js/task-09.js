1e9dd85fadd9e1cc352e517a6e7410c716d14597
Yurii Syvolapenko <svl.yurii@gmail.com>
2023-02-20 10:07:41 +0200

    do task 9

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,11 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const changeColorBtnRef = document.querySelector(".change-color");
+const bodyRef = document.querySelector("body");
+changeColorBtnRef.addEventListener("click", onChangColorBtnClick);
+
+function onChangColorBtnClick() {
+  bodyRef.style.backgroundColor = getRandomHexColor();
+}
