91d32e19afab4ccfaa9ec9b61f50916b2c2a5fb0
Yurii Syvolapenko <svl.yurii@gmail.com>
2023-02-20 09:52:45 +0200

    do task 8

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,13 @@
+const loginFormRef = document.querySelector(".login-form");
+loginFormRef.addEventListener("submit", onLoginFormSubmit);
 
+function onLoginFormSubmit(event) {
+  event.preventDefault();
+  const fieldsValues = {
+    email: loginFormRef.elements.email.value,
+    password: loginFormRef.elements.password.value,
+  };
+  if (fieldsValues.email === "" || fieldsValues.password === "")
+    alert("all fields must be filled");
+  else console.log(fieldsValues);
+}
